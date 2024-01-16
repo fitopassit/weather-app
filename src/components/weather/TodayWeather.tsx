ab5a3d3772f6a5da8b5f491a479f4ed6ffd7a71d
fitopassit <2020-01787@students.ssau.ru>
2024-01-16 23:43:13 +0400

    fix(TodayWeather): Переверстаны компоненты используя модульные стили

--- src/components/weather/TodayWeather.tsx
+++ src/components/weather/TodayWeather.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { WeatherForecast } from './types.ts';
 import {format, parse, isEqual } from 'date-fns';
 import { ru } from 'date-fns/locale';
+import styled from '/src/components/weather/weather.module.css'
 
 const TodayWeather: React.FC<WeatherForecast> = ({...forecast}) => {
   const forecastDay = forecast.dt_txt;
@@ -10,13 +11,13 @@ const TodayWeather: React.FC<WeatherForecast> = ({...forecast}) => {
   const currentDate = format(new Date(), 'yyyy-MM-dd');
 
   return (
-    <div style={{margin: '0 26px', textAlign: 'center'}}>
-      <h3 style={{ color: '#BFBFD4', fontWeight: 'bold', fontSize: '14px' }}>{isEqual(forecastDay, currentDate) ? 'Сегодня': `${dayOfWeek.charAt(0).toUpperCase()}${dayOfWeek.slice(1)}`}</h3>
-      <img style={{ height: '67px', width: '67px' }} src="public/weather/today-weather/cloudy.svg" alt="облачно" />
-      <h3 style={{ color: '#BFBFD4', fontWeight: '400', fontSize: '14px' }}>{forecast.weather[0].description}</h3>
+    <div className={styled.card}>
+      <h3 className={styled.card_weakDay}>{isEqual(forecastDay, currentDate) ? 'Сегодня': `${dayOfWeek.charAt(0).toUpperCase()}${dayOfWeek.slice(1)}`}</h3>
+      <img className={styled.card_img} src="public/weather/today-weather/cloudy.svg" alt="облачно" />
+      <h3 className={styled.card_description}>{forecast.weather[0].description}</h3>
       <div style={{display: 'flex'}}>
-        <h4 style={{ color: '#FAFAFA', fontSize: '14px',marginRight: '8px'}}>{forecast?.main.temp}</h4>
-        <h4 style={{ color: '#7F7F98', fontSize: '14px'}}>{forecast?.main.feels_like}</h4>
+        <h4 className={styled.card_temperature}>{forecast?.main.temp}</h4>
+        <h4 className={styled.card_feelsTemperature}>{forecast?.main.feels_like}</h4>
       </div>
     </div>
   );
